3cf66633cb0209e0da92b7ed341c3ec403dd1304
tendaiwbm <tmbwanda52@gmail.com>
2025-10-15 23:34:45 +0200

    updating infra to work with new folder structure

--- dzimba.sh
+++ dzimba.sh
@@ -6,15 +6,11 @@ if [ "$container" != "$DZIMBA_CONTAINER_NAME" ]; then
 	echo "Creating container.."
 	cd infra
 	./setup.sh $DZIMBA_IMAGE_NAME $DZIMBA_CONTAINER_NAME
+	echo "Finished creating container."
 	cd ..
 	
 	echo "Adding cron job.."
-	touch current_tabs.txt
-	crontab -l > current_tabs.txt
-	echo "DZIMBA_CONTAINER_NAME=$DZIMBA_CONTAINER_NAME" >> current_tabs.txt
-	echo "0-59/15 * * * * sh $PWD/dzimba.sh" >> current_tabs.txt
-        crontab current_tabs.txt
-        rm current_tabs.txt	
+	sh createjob $DZIMBA_CONTAINER_NAME
 fi	
 
 docker start $DZIMBA_CONTAINER_NAME
