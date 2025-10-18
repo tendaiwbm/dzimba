606a6273cf4758182f819fa4a272879707d92796
tendaiwbm <tmbwanda52@gmail.com>
2025-10-18 06:08:15 +0200

    switched send method to fix final email as draft issue

--- mufambi/mail.py
+++ mufambi/mail.py
@@ -1,6 +1,7 @@
 import os
 import smtplib
-from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,7 +14,9 @@ def prepare_data(listings,source_domain_url,path_column_name):
 
 def send_email(pipeline_result):
     data = prepare_data(*pipeline_result.values())
-    message = f"""Subject: Apartments from {list(pipeline_result.keys())[0]} matching your criteria
+    
+    subject = f"NEW - {list(pipeline_result.keys())[0]}"
+    body = f"""
 Hello,
 
 Please see the listings below.
@@ -22,23 +25,28 @@ Please see the listings below.
 
 Ciao
     """
+    
+    message = MIMEMultipart()
+    message["From"] = os.getenv("sender_email_address")
+    message["To"] = os.getenv("recipient_email_address") 
+    message["Subject"] = subject
+    message.attach(MIMEText(body,"plain"))
 
-    atumira = os.getenv("sender_email_address")
-    password = os.getenv("sender_email_password")
-    atumirwa = os.getenv("recipient_email_address") 
     smtpServer = os.getenv("smtp_server")
     smtpPort = os.getenv("smtp_port")
-
     server = smtplib.SMTP(smtpServer,smtpPort)
+
     statusCode, response = server.ehlo()
     print(statusCode,response)
+    
     statusCode, response = server.starttls()
     print(statusCode,response)
-    statusCode, response = server.login(atumira,password)
+    
+    statusCode, response = server.login(os.getenv("sender_email_address"),os.getenv("sender_email_password"))
     print(statusCode,response)
     
     try:
-        server.sendmail(atumira,atumirwa,message)
+        server.send_message(message)
         server.quit()
     except:
         return
