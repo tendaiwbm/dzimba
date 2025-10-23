a4491ceb8b363055c92f10fad9e38624c2d2ef73
tendaiwbm <tmbwanda52@gmail.com>
2025-10-23 04:49:51 +0200

    switched to extracting data from graphql json text instead of dom elements

--- mufambi/pararius/dig.py
+++ mufambi/pararius/dig.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 import requests as rq
 import pandas as p
 from bs4 import BeautifulSoup as BTSP
@@ -7,7 +8,6 @@ from bs4.element import NavigableString
 
 from .model import ParariusModel as model
 from .config import parariusConfig as source
-from .config import geographies
 from ..utils import *
 from ..mail import prepare_data, send_email
 from ..writers import write_json
@@ -49,24 +49,48 @@ def get_create_local_copy(path,filename,data):
     else:
         write_json(data,filepath)
 
-def list_item_to_json(list_item):
-    aTag = list_item.find_all(attrs={"class": "listing-search-item__link listing-search-item__link--depiction"})[0]
-
+def parse_node(node):
     return {
-            "url": aTag.attrs["href"],
-            "_id": aTag.attrs["href"].split("/")[-2]
+             "_id": "-".join(node["item"]["@id"].split("/")[-3:-1]),
+             "url": node["item"]["url"]
            }
+
+def html_innertext_to_json(html):
+    from json import loads
+
+    try:
+        jsonFromText = loads(html.text)
+        assert all(key in jsonFromText for key in ['@context', '@graph'])
+
+        dataContainer = jsonFromText["@graph"][1]
+        assert int(dataContainer["offers"]["offerCount"]) > 0
+
+        data = dataContainer["mainEntity"]["itemListElement"]
+        assert len(data) > 0
+
+        return map(parse_node,data)
+    except:
+        return
+
 
 def request(url,params):
     pageFound = True
     payload = []
-    headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64;x64;rv131.0) Gecko/20100101 Firefox/131.0"}
+    
+    userAgents = [
+                   "Mozilla/5.0 (Windows NT 10.0; Win64;x64;rv131.0) Gecko/20100101 Firefox/131.0",
+                   "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0",
+                   "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
+                 ]
+    
     while pageFound:
         page = "-".join(["page",str(params["page"])])
         encodedUrl = create_path([url,page])
     
-        request = rq.get(encodedUrl)
+        request = rq.get(encodedUrl,headers={"User-Agent": random.choice(userAgents), "Host": "www.pararius.nl"})
         try:
+            # use @graph[offers][offerCount] as termination condition
+            # refactor to attempt termination after parsing dom
             assert len(request.history) == 1
             assert request.history[0].status_code == 301
             print("history check success")
@@ -76,72 +100,60 @@ def request(url,params):
 
         response = request.text
         dom = BTSP(response,"html.parser")
-        
         pageNotFoundContainer = dom.find_all(attrs={"class": "page__notifications"})[0].find_all(attrs={"class": "notification__title"})
+
         if pageNotFoundContainer: 
             break
         
         try:
-            dom = dom.find_all(attrs={"class": "search-list"})
-            assert len(dom) == 1
-            
-            dom = dom[0].find_all(attrs={"class": "search-list__item search-list__item--listing"})
+            dataContainer = dom.find_all(attrs={"type": "application/ld+json"})
+            assert len(dataContainer) == 1
+            dom = dataContainer[0]
         except:
-            pageFound = False
+            break
 
-        dom = map(list_item_to_json,dom)
-        dom = filter(lambda listing: listing != None, dom)
-        payload += dom
-       
+        listings = html_innertext_to_json(dom)
+        if listings:
+            payload += listings
+        
         time.sleep(3)
-
+       
         params["page"] += 1
         
     return payload
 
 def request_geographies(params,urls):
-    listings = [request(url,params) for url in urls]
+    listings = map(lambda url: request(url,params), urls)
     listings = filter(lambda response: bool(response),listings)
     return [listing for subList in listings for listing in subList]
 
-def encode_subgeo_name(name,value):
-    value = value.replace(" ","-").lower()
-    return "-".join([name,value])
+def apply_filters(base_url,filters):
+    assert all(filterGroup in filters for filterGroup in ["city","rentalPrice","datePosted"])
+    
+    cities = [city.lower().replace(" ","-") for city in filters["city"]]
+    random.shuffle(cities)
+    filterUrls = map(lambda city: create_path([base_url,city]), cities)
 
-def encode_subgeo_names(subgeo):
-    subgeoName, subgeoValues = subgeo
-    return map(lambda value: encode_subgeo_name(subgeoName,value),subgeoValues)
+    priceRange = [str(price) for price in filters["rentalPrice"].values()]
+    priceRange = "-".join(priceRange)
+    filterUrls = map(lambda url: create_path([url,priceRange]),filterUrls)
+    
+    filterUrls = map(lambda url: create_path([url,filters["datePosted"]]),filterUrls)
 
-def construct_paths_per_geography(geography):
-    geo = geography[0].replace(" ","-").lower()
-    encodedNames = list(list(map(encode_subgeo_names,geography[1].items()))[0])
-    geoPaths = map(lambda name: create_path([geo,name]),encodedNames)
-    return list(geoPaths)
-
-def construct_geo_urls(domain,endpoint,geographies):
-    geoPaths = list(map(construct_paths_per_geography,geographies.items()))
-    geoPaths =  [path for geo in geoPaths for path in geo]
-    return list(map(lambda geoPath: create_path([domain,endpoint,geoPath]),geoPaths))
-
-def apply_filters(urls,filters):
-    if "rentalPrice" in filters:
-        priceRange = [str(price) for price in filters["rentalPrice"].values()]
-        priceRange = "-".join(priceRange) 
-        urls = map(lambda url: create_path([url,priceRange]),urls)
-
-    if "datePosted" in filters:
-        urls = map(lambda url: create_path([url,filters["datePosted"]]),urls)
-
-    return list(urls)
+    return list(filterUrls)
 
 def validate(data,model):
     validatedData = model.validate(data)
     return dict_rows_to_df(validatedData)
 
 def pipeline():
-    geoUrls = construct_geo_urls(source["domain"],source["endpoint"],geographies)
-    geoUrls = apply_filters(geoUrls,source["filters"])
-    responses = request_geographies(source["requestParams"],geoUrls)
+    baseUrl = create_path([source["domain"],source["endpoint"]])
+    filterUrls = apply_filters(baseUrl,source["filters"])
+    responses = request_geographies(source["requestParams"],filterUrls)
+    
+    if not responses:
+        return False
+
     filteredListings = validate(responses,model)
     knownListings = get_create_local_copy(source["path"],source["localFileName"],filteredListings)
     newListings = extract_new_listings(knownListings,filteredListings,model.id_)
