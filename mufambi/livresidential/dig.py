95981e75d1fc768a450d65216b7c77b188d9d812
tendaiwbm <tmbwanda52@gmail.com>
2025-11-03 06:23:39 +0100

    created new pipeline class; using it for LIVResidential; next=another source?

--- mufambi/livresidential/dig.py
+++ mufambi/livresidential/dig.py
@@ -1,52 +1,11 @@
-import os
 import time
 import json
 import requests as rq
-import pandas as p
-
-from bs4 import BeautifulSoup as BTSP
 
 from .model import LIVResidentialModel as model
 from .config import LivResidentialConfig as source
 from ..utils import *
-from ..writers import write_json
-from ..readers import read_json
 
-def find_new_listing_ids(old,new,unique_id_column):
-    updatedSet = p.CategoricalIndex(new[unique_id_column])
-    previousSet = p.CategoricalIndex(old[unique_id_column])
-    return updatedSet.difference(previousSet)
-
-def extract_new_listings(old,new,unique_id_column):
-    if isinstance(old,type(None)): return new
-
-    newListingIds = find_new_listing_ids(old,new,unique_id_column)
-    
-    if newListingIds.empty: 
-        return
-   
-    newListings = new[new[unique_id_column].isin(newListingIds)]
-
-    return newListings
-
-def update_known_listings(old,new):
-    if isinstance(old,type(None)): return new
-
-    return p.concat([old,new])
-
-def save_known_listings(listings,path,filename):
-    write_json(listings,create_path([path,filename]))     
-
-def dict_rows_to_df(dict_rows):
-    return p.DataFrame(dict_rows)
-
-def get_create_local_copy(path,filename,data):
-    filepath = create_path([path,filename])
-    
-    if filename in os.listdir(path):
-        return dict_rows_to_df(read_json(filepath))
-    else:
-        write_json(data,filepath)
 
 def parse_response(payload):
     def parse_listing(listing):
@@ -73,28 +32,19 @@ def request(url,params,headers):
     
     return response["hits"]
 
-def validate(data,model):
-    validatedData = model.validate(data)
-    return dict_rows_to_df(validatedData)
-
 def pipeline():
-    url = source["dataUrl"]
-    payload = request(url,source["requestParams"],source["headers"])
-     
-    if not(payload):
-        return False
+    pipelineSetup = {
+                      "request": {"function": request, "args": [source["dataUrl"],source["requestParams"],source["headers"]]},
+                      "parser": parse_response,
+                      "directory": source["path"],
+                      "file": source["localFileName"],
+                      "model": model,
+                      "source": source["name"],
+                      "hostUrl": source["domain"],
+                      "listingUrlColumn": model.itemUrl,
+                      "filter": False
+                    }
 
-    response = parse_response(payload)
-    currentListings = validate(response,model)
-    knownListings = get_create_local_copy(source["path"],source["localFileName"],currentListings)
-    newListings = extract_new_listings(knownListings,currentListings,model.id_)
-    
-    try:
-        assert newListings is not None
-    except:
-        return False
-    
-    knownListings = update_known_listings(knownListings,newListings)
-    save_known_listings(knownListings,source["path"],source["localFileName"])
-    
-    return { source["name"]: newListings, "domain": source["domain"], "endpoint": model.itemUrl }
+    worker = Pipeline(pipelineSetup)
+    return worker.execute()
+
--- mufambi/utils.py
+++ mufambi/utils.py
@@ -1,2 +1,93 @@
+import os
+import pandas as p
+
+from .writers import write_json
+from .readers import read_json
+from .mail import send_email
+
 def create_path(parts):
     return "/".join(parts)
+
+def dict_rows_to_df(dict_rows):
+    return p.DataFrame(dict_rows)
+
+class Pipeline:
+    def __init__(self,config):
+        self.config = config
+
+    def request(self):
+        function = self.config["request"]["function"]
+        args = self.config["request"]["args"]
+        return function(*args)
+
+    def parse_payload(self,payload):
+        return self.config["parser"](payload)
+    
+    def validate(self,data):
+        validatedData = self.config["model"].validate(data)
+        return dict_rows_to_df(validatedData)
+
+    def apply_filters(self):
+        return
+
+    def get_create_local_copy(self,data):
+        filepath = create_path([self.config["directory"],self.config["file"]])
+
+        if self.config["file"] in os.listdir(self.config["directory"]):
+            return dict_rows_to_df(read_json(filepath))
+        else:
+            write_json(data,filepath)
+
+    def extract_new_listings(self,old,new):
+        if isinstance(old,type(None)): return new
+        
+        unique_id_column = self.config["model"].id_
+
+        updatedSet = p.CategoricalIndex(new[unique_id_column])
+        previousSet = p.CategoricalIndex(old[unique_id_column])
+        newListingIds = updatedSet.difference(previousSet)
+
+        if newListingIds.empty: return
+
+        newListings = new[new[unique_id_column].isin(newListingIds)]
+
+        return newListings
+
+    def update_known_listings(self,old,new):
+        if isinstance(old,type(None)): return new
+
+        return p.concat([old,new])
+
+    def save_known_listings(self,listings):
+        write_json(listings,create_path([self.config["directory"],self.config["file"]]))
+
+    def email(self,pipeline_result):
+        #assert isinstance(listings,p.DataFrame)
+
+        #listings[path_column_name] = source_domain_url + listings[path_column_name]
+        #listingEndpoints = listings[path_column_name].tolist()
+        #formattedListings = "\n".join(listingEndpoints)
+        
+        send_email(pipeline_result)
+        
+    def execute(self):
+        payload = self.request()
+
+        if not(payload): return False
+
+        response = self.parse_payload(payload)
+        currentListings = self.validate(response)
+
+        if self.config["filter"]:
+            currentListings = self.apply_filters(currentListings)
+
+        knownListings = self.get_create_local_copy(currentListings)
+        newListings = self.extract_new_listings(knownListings,currentListings)
+
+        try:    assert newListings is not None
+        except: return False
+
+        knownListings = self.update_known_listings(knownListings,newListings)
+        self.save_known_listings(knownListings)
+        
+        self.email({ self.config["source"]: newListings, "domain": self.config["hostUrl"], "endpoint": self.config["listingUrlColumn"] })
