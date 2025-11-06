0efc65dca9fa97ab2ca553f9fd31db53af2641d9
tendaiwbm <tmbwanda52@gmail.com>
2025-11-06 05:37:50 +0100

    refactoring Rent An Apartment NL to use Pipeline

--- mufambi/rentanapartmentnl/dig.py
+++ mufambi/rentanapartmentnl/dig.py
@@ -1,4 +1,3 @@
-import os
 import time
 import requests as rq
 import pandas as p
@@ -8,45 +7,6 @@ from bs4.element import NavigableString
 from .model import RAANLModel as model
 from .config import raanlConfig as source
 from ..utils import *
-from ..mail import prepare_data, send_email
-from ..writers import write_json
-from ..readers import read_json
-
-def find_new_listing_ids(old,new,unique_id_column):
-    updatedSet = p.CategoricalIndex(new[unique_id_column])
-    previousSet = p.CategoricalIndex(old[unique_id_column])
-    return updatedSet.difference(previousSet)
-
-def extract_new_listings(old,new,unique_id_column):
-    if isinstance(old,type(None)): return new
-
-    newListingIds = find_new_listing_ids(old,new,unique_id_column)
-    
-    if newListingIds.empty: 
-       return 
-   
-    newListings = new[new[unique_id_column].isin(newListingIds)]
-
-    return newListings
-
-def update_known_listings(old,new):
-    if isinstance(old,type(None)): return new
-
-    return p.concat([old,new])
-
-def save_known_listings(listings,path,filename):
-    write_json(listings,create_path([path,filename]))     
-
-def dict_rows_to_df(dict_rows):
-    return p.DataFrame(dict_rows)
-
-def get_create_local_copy(path,filename,data):
-    filepath = create_path([path,filename])
-    
-    if filename in os.listdir(path):
-        return dict_rows_to_df(read_json(filepath))
-    else:
-        write_json(data,filepath)
 
 def remove_from_list(item,list_object):
     while item in list_object:
@@ -113,9 +73,6 @@ def request(url,params):
         
         if not(pageFound): break
 
-        dom = map(dom_article_to_json,dom)
-        dom = filter(lambda listing: listing != None, dom)
-
         time.sleep(1)
 
         payload += dom
@@ -123,48 +80,29 @@ def request(url,params):
 
     return payload
     
-def validate(data,model):
-    validatedData = model.validate(data)
-    return dict_rows_to_df(validatedData)
-
-def apply_filters(data,filters,model):
-    print(f"Number of listings before filtering:\t{len(data)}")
-    message = "Number of listings after applying a filter on '{}':\t{}"
-
-    if "city" in filters:
-        data = data[data[model.city].isin(filters["city"])]
-        print(message.format("city",len(data)))
-    
-    if "rentalPrice" in filters:
-        data = data[(data[model.rentalPrice] >= filters["rentalPrice"]["min"]) & 
-                    (data[model.rentalPrice] <= filters["rentalPrice"]["max"])]
-        print(message.format("rentalPrice",len(data)))
-     
-    if "energyLabel" in filters:
-        try:
-            assert all(p.notna(data[model.energyLabel]))
-            data = data[data[model.energyLabel].isin(filters["energyLabel"])]
-            print(message.format("energyLabel",len(data)))
-        except:
-            pass
-    
-    return data
-
+def parse_response(payload):
+    dom = map(dom_article_to_json,payload)
+    dom = filter(lambda listing: listing != None, dom)
+    return list(dom)
 
 def pipeline():
-    url = create_path([source["domain"],source["endpoint"]])
-    response = request(url,source["requestParams"])
-    currentListings = validate(response,model)
-    filteredListings = apply_filters(currentListings,source["filters"],model)
-    knownListings = get_create_local_copy(source["path"],source["localFileName"],filteredListings)
-    newListings = extract_new_listings(knownListings,filteredListings,model.id_)
+    pipelineSetup = {
+                      "request": {
+                                   "function": request,
+                                   "args": [
+                                             create_path([source["domain"],source["endpoint"]]),
+                                             source["requestParams"]
+                                           ]
+                                 },
+                      "parser": parse_response,
+                      "directory": source["path"],
+                      "file": source["localFileName"],
+                      "model": model,
+                      "source": source["name"],
+                      "hostUrl": source["domain"],
+                      "listingUrlColumn": model.itemUrl,
+                      "filter": source["filters"]
+                    }
 
-    try:
-        assert newListings is not None
-    except:
-        return False
-
-    knownListings = update_known_listings(knownListings,newListings)
-    save_known_listings(knownListings,source["path"],source["localFileName"])
-    
-    return { source["name"]: newListings, "domain": source["domain"], "endpoint": model.itemUrl }
+    worker = Pipeline(pipelineSetup)
+    return worker.execute()
