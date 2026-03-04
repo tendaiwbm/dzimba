06cee320dd4f98f27003a5ba004197e55189d12d
tendaiwbm <tmbwanda52@gmail.com>
2026-03-05 00:34:10 +0100

    moving cron script to infra/

---
+++ infra/setup-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./remove.sh
+docker build --no-cache -t $DZIMBA_IMAGE_NAME -f Dockerfile .
+docker create --name $DZIMBA_CONTAINER_NAME --mount type=bind,source=..,target=/app -p 8085:8085 $DZIMBA_IMAGE_NAME
