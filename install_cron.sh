b44baf2842956df3ce3020c15bd2303a6a43ca7d
tendaiwbm <tmbwanda52@gmail.com>
2026-03-04 22:25:45 +0100

    run only with cron & log everything to logs.txt

--- install_cron.sh
+++ install_cron.sh
@@ -3,7 +3,7 @@
 
 touch current_tabs.txt
 crontab -l > current_tabs.txt
-echo "DZIMBA_CONTAINER_NAME=$1" >> current_tabs.txt
-echo "0-59/5 * * * * sh $PWD/dzimba.sh" >> current_tabs.txt
+echo "DZIMBA_CONTAINER_NAME=$DZIMBA_CONTAINER_NAME" >> current_tabs.txt
+echo "0-59/5 * * * * docker start $DZIMBA_CONTAINER_NAME >> logs.txt 2>&1" >> current_tabs.txt
 crontab current_tabs.txt
 rm current_tabs.txt
